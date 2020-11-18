3d6f0a1b20554f4fa67dd9a808dd79156342e58f
Ilya Yereferenko <ilyeref@gmail.com>
2020-11-19 05:03:02 +0600

    Fixed initial clean script

--- analysis/scripts/initial_clean.py
+++ analysis/scripts/initial_clean.py
@@ -12,7 +12,3 @@ def load_prep(url):
             .drop(columns=["version", "FIPS", "state", "office"])
     )
     return df
-
-
-print(load_prep(
-    "/Users/gor154/Documents/Workspaces.nosync/COSC 301 Git/course-project-solo_125/data/raw/countypres_2000-2016.csv"))
